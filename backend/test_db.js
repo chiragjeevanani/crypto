c0b250797c01feb9fc71fbcae022546534d1c946
sheetalchouhan689 <sheetalchouhan689@gmail.com>
2026-05-28 18:31:59 +0530

    uri

--- backend/test_db.js
+++ backend/test_db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const NFTOwnership = require('./models/NFTOwnership');
 const Auction = require('./models/Auction');
 
-mongoose.connect('mongodb+srv://sheetaldatacode_db_user:BFSw6i820156rrjl@kqreels.zljevvq.mongodb.net/crypto-appzeto?retryWrites=true&w=majority&appName=kqreels').then(async () => {
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI).then(async () => {
     const ownerships = await NFTOwnership.find();
     console.log("ALL OWNERSHIPS:", ownerships);
     const auctions = await Auction.find({ nftStatus: { $ne: "not_started" } });
